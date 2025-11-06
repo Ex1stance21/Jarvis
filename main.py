cb91d298eb2e27c254e0671b0e450854d905d091
Anton <antonandreev189@gmail.com>
2025-11-06 12:35:05 +0200

    Добавив голос джарвісу

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
-import pvporcupine
+import os, pvporcupine, random, pygame
 import sounddevice as sd
-import numpy as np
-import time
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
-class WakeWordDetector:
-    def __init__(self, access_key, keyword_paths, sensitivities):
-        self.porcupine = pvporcupine.create(
-            access_key=access_key,
-            keyword_paths=keyword_paths,
-            sensitivities=sensitivities,
-        )
+pygame.mixer.init()
+Hi_ser = pygame.mixer.Sound('Audio\\Hi_sir.mp3')
+Yes_ser = ['Audio\\Yes_sir.mp3',
+           'Audio\\Yes_sir2.mp3',
+           'Audio\\Yes_sir3.mp3']
+Hi_ser.play()
+sd.wait()
 
-    def callback(self, indata, frames, time, status):
-        pcm = indata[:, 0]
-        if self.porcupine.process(pcm) >= 0:
-            print("Detected")
-
-    def mic_on(self):
-        with sd.InputStream(
-                samplerate=self.porcupine.sample_rate,
-                blocksize=self.porcupine.frame_length,
-                channels=1,
-                dtype='int16',
-                callback=self.callback
-        ):
-            input("Натисніть Enter для завершення...\n")
-
-    def start(self):
-        self.mic_on()
-
-detector = WakeWordDetector(
-    'q+ZjIphUWl1mPiAvhU9MiB7wrkS3OTmG+O49Ed7dSPMRRpr2xHaXBQ==',
-    [r'Models\Jarvis_wake_word.ppn'],
-    [1]
+handler = pvporcupine.create(
+    access_key='q+ZjIphUWl1mPiAvhU9MiB7wrkS3OTmG+O49Ed7dSPMRRpr2xHaXBQ==',
+    keyword_paths=[r'Models\Jarvis_wake_word.ppn'],
+    sensitivities=[1]
 )
-detector.start()
+
+start_time = None
+
+def callback(indata, frames, time_info, status):
+    pcm = indata[:, 0]
+
+    if handler.process(pcm) >= 0:
+        Random_sound = random.choice(Yes_ser)
+        sound = pygame.mixer.Sound(Random_sound)
+        sound.play()
+
+def mic_on():
+    with sd.InputStream(
+            samplerate=handler.sample_rate,
+            blocksize=handler.frame_length,
+            channels=1,
+            dtype='int16',
+            latency='low',
+            callback=callback
+    ):
+        input("Натисніть Enter для завершення...\n")
+
+mic_on()
+
+handler.delete()
 
